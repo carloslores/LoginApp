94e42b918a37518cc251de7c6ffa539fe2d644b7
Carlos Lores <carlos.lores@tacubayaconsulting.com>
2020-04-07 17:24:26 +0200

    primer commit en develop

---
+++ LoginApp/src/app/pages/login/login.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+import { UserModel } from '../../models/user.model';
+import { NgForm } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+import Swal from 'sweetalert2';
+import {Router} from "@angular/router";
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  user: UserModel;
+  remeber = false;
+  constructor( private auth: AuthService,
+               private router: Router) { }
+
+  ngOnInit() {
+    this.user = new UserModel();
+  }
+
+  login(form: NgForm) {
+    if (form.invalid) {
+      return;
+    }
+    Swal.fire({
+      allowOutsideClick: false,
+      icon: 'info',
+      text: 'Espere por favor...'
+    })
+    Swal.showLoading();
+    this.auth.loging(this.user)
+    .subscribe( res => {
+      console.log(res);
+      Swal.close();
+      if (this.remeber) {
+        localStorage.setItem('email', this.user.email);
+      }
+      this.router.navigateByUrl('/home');
+    }, err => {
+      console.log(err.error.error.message);
+      Swal.fire({
+        icon: 'error',
+        title: 'Error al autenticar',
+        text: err.error.error.message
+      });
+    });
+  }
+}
---
+++ LoginApp/src/app/pages/registro/registro.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import { UserModel } from 'src/app/models/user.model';
+import { NgForm } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+import Swal from "sweetalert2";
+import {Router} from "@angular/router";
+
+@Component({
+  selector: 'app-registro',
+  templateUrl: './registro.component.html',
+  styleUrls: ['./registro.component.css']
+})
+export class RegistroComponent implements OnInit {
+  user: UserModel;
+
+  constructor( private auth: AuthService,
+               private router: Router) { }
+
+  ngOnInit() {
+    this.user = new UserModel();
+   }
+
+  onSubmit(form: NgForm) {
+    if (form.invalid) {
+      return;
+    }
+
+    Swal.fire({
+      allowOutsideClick: false,
+      icon: 'info',
+      text: 'Espere por favor...'
+    })
+    Swal.showLoading();
+    this.auth.newUser(this.user)
+    .subscribe( resp =>{
+      console.log(resp);
+      Swal.close();
+      this.router.navigateByUrl('/home');
+    }, err => {
+      console.log(err.error.error.message);
+      Swal.fire({
+        icon: 'error',
+        title: 'Error de registro',
+        text: err.error.error.message
+      });
+    });
+  }
+}
